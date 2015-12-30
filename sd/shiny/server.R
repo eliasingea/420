564e91d1e0061f2945f846e9c156a11293815d08
Stephen <stephen@umw.edu>
2015-12-29 22:50:12 -0800

    Add shiny interface scaffolding.
    Add bathtub example to shiny.

---
+++ sd/shiny/server.R
@@ -0,0 +1,28 @@
+
+library(shiny)
+
+source("../bathtub.R")
+
+shinyServer(function(input,output,session) {
+
+    output$bathtubWaterLevelPlot <- renderPlot({
+        sim.results <- bathtub.sim(init.water.level.gal=input$initWaterLevel,
+            turn.on.faucet.time=input$faucetOnOff[1]*60,
+            turn.off.faucet.time=input$faucetOnOff[2]*60,
+            pull.plug.time=input$pullPlugTime*60,
+            inflow.rate=input$inflowRate/60,
+            outflow.rate=input$outflowRate/60,
+            sim.length=input$bathtubSimLength)
+        plot.bathtub.water.level(sim.results)
+    })
+    
+    output$faucetOnOffWidget <- renderUI({
+        sliderInput("faucetOnOff", "Turn on/off faucet time (min)",
+            min=0, max=input$bathtubSimLength, step=.1, value=c(1,4))
+    })
+
+    output$pullPlugTimeWidget <- renderUI({
+        sliderInput("pullPlugTime", "Pull plug time (min)",
+            min=0, max=input$bathtubSimLength, step=.1, value=2)
+    })
+})
---
+++ sd/shiny/ui.R
@@ -0,0 +1,36 @@
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+    tags$head(tags$link(rel="stylesheet", type="text/css", href="shiny.css")),
+
+    titlePanel("CPSC 420 -- System Dynamics examples"),
+
+    tabsetPanel(selected="Bathtub",
+        tabPanel("Bathtub",
+            sidebarLayout(sidebarPanel(
+                numericInput("bathtubSimLength", "Simulation time (min)",
+                    value=5, min=0, step=.5, width="40%"),
+                sliderInput("initWaterLevel", "Initial water level (gal)",
+                    min=0, max=60, value=50),
+
+                # (Putting the code for these into server.R is necessary so
+                # their width can vary dynamically dependent on the
+                # bathtubSimLength widget's value.)
+                uiOutput("faucetOnOffWidget"),
+                uiOutput("pullPlugTimeWidget"),
+
+                numericInput("inflowRate", "Faucet flow rate (gal/min)",
+                    value=15, min=0, step=1, width="45%"),
+                numericInput("outflowRate", "Drain rate (gal/min)",
+                    value=20, min=0, step=1, width="45%")
+                ),
+                mainPanel(
+                    plotOutput("bathtubWaterLevelPlot")
+                )
+            )
+        )
+    )
+
+))
